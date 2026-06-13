364b64477ccefedb7e370ffbe93e1882fa6f44e7
umar abbas <umarshah6444@gmail.com>
2026-06-13 23:59:34 +0500

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -1,11 +1,4 @@
 # My first Python file from Colab
 print("Hello from Google Colab!")
 
-def hello_world():
-    return "Pushed to GitHub successfully!"
 
-# Now push it
-%cd /content/machine-learning
-!git add first_file.py
-!git commit -m "Initial commit: Add first Python file"
-!git push origin main
